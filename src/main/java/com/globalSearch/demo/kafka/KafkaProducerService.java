11376d6aa8a0bbb966ec5e38ffea7a3e77f42d13
MayankSrivastava989 <mayank.srivastava@appinventiv.com>
2023-10-09 14:20:24 +0530

    added configuration for kafka

--- src/main/java/com/globalSearch/demo/kafka/KafkaProducerService.java
+++ src/main/java/com/globalSearch/demo/kafka/KafkaProducerService.java
@@ -3,7 +3,6 @@ package com.globalSearch.demo.kafka;
 import com.globalSearch.demo.entity.Customer;
 import com.globalSearch.demo.entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
